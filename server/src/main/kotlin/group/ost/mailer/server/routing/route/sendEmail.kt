02c55e5d90860e181372ee8528f857b559da6c54
Alexandr Kovalenko <destructsymp@gmail.com>
2024-07-09 16:11:52 +0300

    Правки эндпоинта.

--- server/src/main/kotlin/group/ost/mailer/server/routing/route/sendEmail.kt
+++ server/src/main/kotlin/group/ost/mailer/server/routing/route/sendEmail.kt
@@ -1,23 +1,53 @@
 package group.ost.mailer.server.routing.route
 
 import group.ost.mailer.domain.service.MailerService
+import group.ost.mailer.server.database.table.UserEmailTable
 import group.ost.mailer.server.request.SendEmailRequest
 import group.ost.mailer.server.extension.getAPIConfiguration
-import group.ost.mailer.server.response.SendEmailMessageResponse
+import group.ost.mailer.server.response.SendEmailResponse
 import group.ost.mailer.server.extension.sendingDto
 import group.ost.mailer.server.util.getFileLogger
+import group.ost.mailer.server.vo.SendingReason
 import io.ktor.http.*
-import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
 
-fun Route.sendEmail() = authenticate {
+fun Route.sendEmail() {
     val apiVersion = getAPIConfiguration().version
 
     post<SendEmailRequest>("/$apiVersion/mailer/sendEmail") { request ->
         val service by inject<MailerService>()
         val dto = request.sendingDto
+
+        val row = UserEmailTable.getByEmail(request.receiverEmailAddress)
+
+        when (request.reason) {
+            SendingReason.registration,
+            SendingReason.change -> {
+                if (row != null) {
+                    return@post call.respond(
+                        status = HttpStatusCode.BadRequest,
+                        message = SendEmailResponse(
+                            email = request.receiverEmailAddress,
+                            error = SendEmailResponse.Error.EMAIL_ALREADY_EXISTS
+
+                        )
+                    )
+                }
+            }
+            SendingReason.restorePassword -> {
+                if (row == null) {
+                    return@post call.respond(
+                        HttpStatusCode.BadRequest,
+                        SendEmailResponse(
+                            email = request.receiverEmailAddress,
+                            error = SendEmailResponse.Error.EMAIL_NOT_FOUND
+                        )
+                    )
+                }
+            }
+        }
 
         runCatching {
             service
@@ -26,7 +56,7 @@ fun Route.sendEmail() = authenticate {
         }
             .onSuccess {
                 return@post call.respond(
-                    message = SendEmailMessageResponse(
+                    message = SendEmailResponse(
                         email = request.receiverEmailAddress
                     )
                 )
@@ -36,16 +66,15 @@ fun Route.sendEmail() = authenticate {
                     .error {
                         (
                             (exc.message ?: exc.cause?.message)
-                            ?.let { "" }
-                            ?: "Неизвестная ошибка при попытке отправить письмо на почтовый адрес ${dto.toAddress}\n"
-                        ) + exc.stackTraceToString()
+                                ?: "Неизвестная ошибка при попытке отправить письмо на почтовый адрес ${dto.toAddress}\n"
+                            ) + "\n" + exc.stackTraceToString()
                     }
 
                 return@post call.respond(
                     status = HttpStatusCode.InternalServerError,
-                    message = SendEmailMessageResponse(
+                    message = SendEmailResponse(
                         email = request.receiverEmailAddress,
-                        error = SendEmailMessageResponse.Error.EMAIL_SENDING_FAILED
+                        error = SendEmailResponse.Error.SENDING_FAILED
                     )
                 )
             }
